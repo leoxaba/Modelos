2a52d8aa8a917479f9a47648d54d93dc822f15ae
L3nx Studio <118389990+leoxaba@users.noreply.github.com>
2022-12-07 14:55:55 -0300

    Update enviar_email_automático.py

--- Automação_emails/enviar_email_automático.py
+++ Automação_emails/enviar_email_automático.py
@@ -8,19 +8,19 @@ from email.mime.text import MIMEText
 # S M T P - Simple mail transfer protocol
 # Para cria o servidor e enviar email
 
-#1- Startar o servidor SMTP
+# 1- Startar o servidor SMTP
 
 host = 'smtp.furg.br'
 port = '587'
 login = 'leonardo.schabbach@furg.br'
-senha = '*****'
+senha = '******1'
 
 server = smtplib.SMTP(host, port)
 server.ehlo()
 server.starttls()
-server.login(login,senha)
+server.login(login, senha)
 
-#2- Construir o email tipo MIME
+# 2- Construir o email tipo MIME
 
 corpo = "olah Mundo!"
 
@@ -28,12 +28,11 @@ corpo = "olah Mundo!"
 email_msg = MIMEMultipart()
 email_msg['From'] = login
 email_msg['To'] = 'arquivorh@furg.br'
-email_msg['Subject'] = "E-mail automático do Léo"
+email_msg['Subject'] = "E-mail automático do L3nx-Studio"
 email_msg.attach(MIMEText(corpo, 'plain'))
 
 
-
-#3- Enviar o email tipo MIME no servidor SMTP
+# 3- Enviar o email tipo MIME no servidor SMTP
 
 server.sendmail(email_msg['From'], email_msg['To'], email_msg.as_string())
 server.quit()
